323224eee654ddaa062a7eac978b51ae800f6f82
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-13 18:27:02 +0330

    deleteTask

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
 console.clear();
 import "dotenv/config";
 import DB from "./db.js";
+
+const data1 = [
+  {
+    id: 1,
+    title: "Learn C#",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Learn MATLAB",
+    completed: false,
+  },
+];
+const data2 = '[{"id":1,"title":"Learn Java","completed":true}]';
+DB.insertBulkData(data1);
+
+DB.deleteTask(1);
